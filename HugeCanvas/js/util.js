c0ad56c420daa5d3492b496f154acd18ae524baf
田不大白 <1127114376@qq.com>
2017-02-28 14:24:52 +0800

    Update util.js
    基本完成版

--- HugeCanvas/js/util.js
+++ HugeCanvas/js/util.js
@@ -2,7 +2,26 @@
   var util = {
     calcRange: function (objs) {
       // 计算一堆物体占有的空间
-
+      var minX, minY,
+          maxX, maxY;
+      objs.map(function (obj) {
+        minX = util.min(minX, obj.args[0]);
+        minY = util.min(minY, obj.args[1]);
+        maxX = util.max(maxX, obj.args[0] + obj.args[2]);
+        maxY = util.max(maxY, obj.args[1] + obj.args[3]);
+      });
+      return [
+        minX - 5,
+        minY - 5,
+        maxX - minX + 5,
+        maxY - minY + 5
+      ]
+    },
+    max: function (a, b) {
+      return a > b ? a : b;
+    },
+    min: function (a, b) {
+      return a < b ? a : b;
     }
   }
 
