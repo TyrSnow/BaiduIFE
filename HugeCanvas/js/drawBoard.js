72d6dd81b6f764aad790dcdcf0534b3557f9fd00
田不大白 <1127114376@qq.com>
2017-02-28 14:22:55 +0800

    Update drawBoard.js
    基本完成版

--- HugeCanvas/js/drawBoard.js
+++ HugeCanvas/js/drawBoard.js
@@ -10,8 +10,8 @@
   function DrawBoard(opt) {
     this.canvas = opt.elem;
     this.viewer = opt.viewer;
-    this.width = this.canvas.width;
-    this.height = this.canvas.height;
+    this.width = this.canvas.width = opt.width;
+    this.height = this.canvas.height = opt.height;
   }
   DrawBoard.prototype.bindEvents = function (events) {
 
@@ -21,10 +21,11 @@
     //
   }
   function checkPointVisible(pt, width, height) {
-    if (pt[0] < 0 || pt[1] < 0 || pt[0] > width || pt[1] > height) {
-      return false;
-    } else {
+    // 在框里面返回true
+    if (pt[0] >= 0 && pt[1] >= 0 && pt[0] <= width && pt[1] <= height) {
       return true;
+    } else {
+      return false;
     }
   }
   function checkObjVisible(pts, width, height) {
@@ -33,40 +34,65 @@
     // 都不在框中返回false
     var flag = false;
     pts.map(function (pt) {
-      flag = checkPointVisible(pt, width, height);
+      flag = flag || checkPointVisible(pt, width, height);
     })
     return flag;
   }
   DrawBoard.prototype.calcObjArgs = function (obj) {
     // 根据viewer测算新的参数值
     var args = [];
-    args[0] = obj.args[0] - this.viewer.pos[0];
-    args[1] = obj.args[1] - this.viewer.pos[1];
-    args[2] = obj.args[2] * this.viewer.scale;
-    args[3] = obj.args[3] * this.viewer.scale;
+    args[0] = (obj.args[0] - this.viewer.pos[0]) * this.viewer.scale;
+    args[1] = (obj.args[1] - this.viewer.pos[1]) * this.viewer.scale;
+    args[2] = util.max(obj.args[2] * this.viewer.scale, 1); // 无论缩放到什么程度都至少占一个像素？似乎没效果？
+    args[3] = util.max(obj.args[3] * this.viewer.scale, 1);
     if (checkObjVisible([
       [args[0], args[1]],
       [args[0], args[1] + args[3]],
       [args[0] + args[2], args[1]],
       [args[0] + args[2], args[1] + args[3]],
     ], this.width, this.height)) {
-      return false;
+      return args;
     }
-    return args;
+    return false;
   }
   DrawBoard.prototype.drawObj = function (ctx, obj) {
     // 判断obj的类型，绘图
     // 先重新计算obj对于视口的参数
     var args = this.calcObjArgs(obj);
     if (args === false) {
-      console.log('Not Draw.')
       return;
     }
-    ctx.fillStyle = '#FF0000';
-    ctx.fillRect.apply(ctx, args);
+    ctx.fillStyle = obj.fillStyle || '#FF0000';
+    if (obj.fill) {
+      ctx.fillRect.apply(ctx, args);
+    } else {
+      ctx.strokeRect.apply(ctx, args);
+    }
+
+  }
+  // 将当前视口的坐标转换成画板的坐标
+  DrawBoard.prototype.translationPos = function (pos) {
+    var _pos = [
+      (pos[0] + this.viewer.pos[0]) / this.viewer.scale,
+      (pos[1] + this.viewer.pos[1]) / this.viewer.scale
+    ];
+    return _pos;
+  }
+  /* 把view的参数传出去*/
+  DrawBoard.prototype.getViewObj = function () {
+    return {
+      type: 'view',
+      fillStyle: '#F0F',
+      fill: false,
+      args: [
+        this.viewer.pos[0],
+        this.viewer.pos[1],
+        this.width / this.viewer.scale,
+        this.height / this.viewer.scale
+      ]
+    }
   }
   DrawBoard.prototype.render = function (objs) {
-    // render会把prepare准备好的页面绘制到页面上
     var _this = this;
     var ctx = this.canvas.getContext('2d');
     ctx.clearRect(0, 0, this.width, this.height); // 清空画布
@@ -74,15 +100,34 @@
       // 这里的obj是有id的，
       _this.drawObj(ctx, obj);
     });
-    // 启动prepare准备下一帧
-
   }
   // 下面是控制类的函数
   DrawBoard.prototype.zoomIn = function () {
     this.viewer.scale+= 0.1;
   }
   DrawBoard.prototype.zoomOut = function () {
-    this.viewer.scale-= 0.1;
+    // 最多缩小到0.1
+    if (this.viewer.scale > 0.1) {
+      this.viewer.scale-= 0.1;
+    } else {
+      message('不能再缩小了！');
+    }
+  }
+  DrawBoard.prototype.setZoom = function (scale) {
+    this.viewer.scale = scale;
+  }
+  DrawBoard.prototype.move = function (derect) {
+    this.viewer.pos = [
+      this.viewer.pos[0] - derect[0],
+      this.viewer.pos[1] - derect[1]
+    ];
+  }
+  DrawBoard.prototype.moveTo = function (pos) {
+    this.viewer.pos = [
+      pos[0],
+      pos[1]
+    ];
   }
   window.DrawBoard = DrawBoard;
 })();
+
